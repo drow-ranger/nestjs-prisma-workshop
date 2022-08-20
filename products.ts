d1340b45367def5a1bc423912b379c96a9af33f9
Drow Ranger <nico98deo@gmail.com>
2022-08-21 02:34:38 +0700

    init

---
+++ products.ts
@@ -0,0 +1,117 @@
+// database product seed
+export const products = [
+  {
+    name: 'Sustainable [Type A] [Regular Edition] [CD+DVD]',
+    description:
+      'AKB48 brings the 56th single. This edition includes a bonus DVD with two music videos (subject to change). Also comes with a photo randomly selected from several kinds.',
+    price: 11.3,
+    sku: 'sku_dvd_jpop_10',
+    published: true,
+  },
+  {
+    name: 'Labrador Retriever [Type A / CD+DVD / Regular Edition]',
+    description:
+      "Single from AKB48 features a summer tune and two more new songs (Third tracks of type A, K, B, and 4 are different from each other). Comes with a bonus DVD featuring music videos of the three new songs and more bonus video footage (subject to change). This edition includes a randomly selected photo and a serial-numbered card to vote for 'AKB48 37th Single Senbatsu Sosenkyo' for a limited period of time. *The DVD disc is encoded for region 2 (Japan, Europe, and Middle East), and no subtitles are included.",
+    price: 11.3,
+    sku: 'sku_dvd_jpop_12',
+    published: true,
+  },
+  {
+    name: 'Kuchibiru ni Be My Baby [Type A / CD+DVD / Regular Edition]',
+    description:
+      "AKB48 42nd single containing '365 Nichi no Kami Hikoki,' the main theme of the popular TV series 'Asa ga Kita' that was broadcast on NHK between September 28, 2015 and April 2, 2016. Comes with a bonus DVD featuring music videos for four new songs and more bonus video footage. Regular edition comes with a randomly selected photo. Also comes with two voting cards each for 'Set List Best 100 2016' & 'AKB48 One-Group Request Hour' (system requirements unknown). Comes with an application card (valid only in Japan). *The first and the second tracks in CD and DVD in all the types are the same each other.",
+    price: 11.3,
+    sku: 'sku_dvd_jpop_17',
+    published: true,
+  },
+  {
+    name: 'TODAY IS ANOTHER DAY',
+    description: 'TODAY IS ANOTHER DAY / ZARD',
+    price: 21.6,
+    sku: 'sku_dvd_jpop_7',
+    published: false,
+  },
+  {
+    name: 'Good-bye My Loneliness',
+    description: 'Six-song release from Zard.',
+    price: 15.84,
+    sku: 'sku_dvd_rock_10',
+    published: true,
+  },
+  {
+    name: 'ZARD Streaming Live "What a beautiful memory - 30th Anniversary -"',
+    description:
+      'Features three concerts held by ZARD (the first online concert distributed on February 10, 2021 from Tokyo International Forum, the first acoustic live held at Kodaiji, Kyoto on May 27th, and excerpts from the first symphonic concert held at Tokyo Bunka Kaikan (nine tracks)). Comes with a 40-page booklet. Slipcase packaging.',
+    price: 51.11,
+    sku: 'sku_dvd_jpop_20',
+    published: true,
+  },
+  {
+    name: 'Fushigine...',
+    description:
+      'Thirtieth anniversary release of ZARD is 30-title reissue series in 12cm single format! Features the latest remastering.',
+    price: 6.59,
+    sku: 'sku_dvd_rock_2',
+    published: false,
+  },
+  {
+    name: 'Negaigoto no Mochigusare [Type A] [Regular Edition] [CD+DVD]SUP Paddle Kids',
+    description:
+      'AKB48 brings the 48th single. Comes with a DVD featuring three music videos each for a new song. This edition includes a photo randomly selected from several kinds. Also comes with a voting card for their general election for the 49th single for a limited period of time (system requirements unknown). *The 1st and 2nd tracks corresponding to all the types are the same.',
+    price: 11.13,
+    sku: 'sku_dvd_jpop_21',
+    published: true,
+  },
+  {
+    name: '"Dragon Ball" Anime 30th Anniversary: Dragon Ball Kami BEST [Regular Edition]',
+    description:
+      'Greatest hits album release celebrating "Dragon Ball" series 30th anniversary. Features opening/ending themes as "CHA-LA HEAD-CHA-LA" by Hironobu Kageyama and much more.',
+    price: 20.28,
+    sku: 'sku_dvd_anime_4',
+    published: true,
+  },
+  {
+    name: 'CYCLE HIT 1991-1997 Spitz Complete Single Collection [Regular Edition]',
+    description:
+      'CYCLE HIT 1991-1997 Spitz Complete Single Collection [通常盤] / スピッツ',
+    price: 18.25,
+    sku: 'sku_dvd_jpop_24',
+    published: true,
+  },
+  {
+    name: 'Teacher Teacher [Type A] [w/ Event Ticket, Limited Edition] [CD+DVD]',
+    description:
+      'AKB48 brings the 52nd single. Comes with a bonus DVD featuring music videos of three new songs (subject to change). Limited edition comes with an event ticket (valid only in Japan) and a voting code for their world general election for the 53rd single for a limited period of time (system requirements unknown).',
+    price: 11.13,
+    sku: 'sku_dvd_jpop_32',
+    published: true,
+  },
+  {
+    name: 'Jabaja [Type A] [w/ Event Ticket, Limited Edition] [CD+DVD]',
+    description:
+      'AKB48 brings the first single in 2018. Comes with a DVD with three music videos (subject to change). This edition comes with a ticket for an event (valid only in Japan).',
+    price: 11.13,
+    sku: 'sku_dvd_jpop_33',
+    published: true,
+  },
+  {
+    name: 'Ueki no Hosoku (The Law of Ueki) - Original Soundtrack - The Law Of Music! [Shipping Within Japan Only]',
+    description:
+      'ATTENTION!!! Due to publisher restriction, this product is no longer available for any order to be shipped outside of Japan (restriction applied starting from 6PM on February 15, 2017).',
+    price: 21.27,
+    sku: 'sku_dvd_anime_8',
+    published: false,
+  },
+  {
+    name: 'Jikochu de Iko [CD+DVD / Type A]',
+    price: 11.16,
+    sku: 'sku_dvd_jpop_34',
+    published: true,
+  },
+  {
+    name: 'Kimi no Na wa Kibo [CD+DVD/Type-A]',
+    price: 11.13,
+    sku: 'sku_dvd_jpop_35',
+    published: false,
+  },
+];
