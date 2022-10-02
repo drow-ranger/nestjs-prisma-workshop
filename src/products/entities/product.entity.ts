8388c6fdcc05bdabe7e1acb38184c8f4d55b9cad
drow-ranger <nico98deo@gmail.com>
2022-10-02 23:57:53 +0700

    create ProductEntity

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,28 @@
-export class Product {}
+import { Product, Prisma } from '@prisma/client';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class ProductEntity implements Product {
+  @ApiProperty()
+  id: string;
+
+  @ApiProperty()
+  createdAt: Date;
+
+  @ApiProperty()
+  updatedAt: Date;
+
+  @ApiProperty()
+  name: string;
+
+  @ApiProperty({ required: false, nullable: true })
+  description: string | null;
+
+  @ApiProperty({ type: String })
+  price: Prisma.Decimal;
+
+  @ApiProperty()
+  sku: string;
+
+  @ApiProperty({ default: false })
+  published: boolean;
+}
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -2,7 +2,8 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { ProductEntity } from './entities/product.entity';
 
 @Controller('products')
 @ApiTags('products')
@@ -10,6 +11,7 @@ export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
+  @ApiOkResponse({ type: [ProductEntity] })
   create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
@@ -25,11 +27,13 @@ export class ProductsController {
   }
 
   @Patch(':id')
+  @ApiCreatedResponse({ type: ProductEntity })
   update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productsService.update(id, updateProductDto);
   }
 
   @Delete(':id')
+  @ApiOkResponse({ type: ProductEntity })
   remove(@Param('id') id: string) {
     return this.productsService.remove(id);
   }
